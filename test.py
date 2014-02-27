1a3a23c5fdc3e31a4cb0aa444506b98d36e05d72
Kalle Hallivuori <kato@iki.fi>
2014-02-27 22:06:45 +0200

    initial version of test

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import game
+
+def will(expected, actual, assumption):
+    if expected != actual:
+        raise UserWarning("%s: expected '%s', got '%s'" % (assumption, expected, actual))
+    print('%s ok' % assumption)
+
+g = Game()
+will('You are at start', g.start(), 'start output')
+will('You cannot act unexpectedly', g.do('act unexpectedly'), 'unexpected action handling')
+will('You go to end', g.do('go to end'), 'expected action')
+
