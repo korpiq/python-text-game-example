f11d5703d187cafcbbfcf4ae5ddc7395464d6678
Kalle Hallivuori <kato@iki.fi>
2014-02-27 22:15:07 +0200

    First three tests pass but game is not yet playable.

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
+actions = {
+    'go to end': 'You go to end'
+}
 
 class Game:
     def start(self):
 	return 'You are at start'
 
     def do(self, action):
+        if action in actions:
+            return actions[action]
         return 'You cannot act unexpectedly' 
+
