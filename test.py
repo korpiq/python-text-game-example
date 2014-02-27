e508bb28dc45f8648b17367e44436fdb38689489
Kalle Hallivuori <kato@iki.fi>
2014-02-27 22:08:05 +0200

    empty Game class

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def will(expected, actual, assumption):
         raise UserWarning("%s: expected '%s', got '%s'" % (assumption, expected, actual))
     print('%s ok' % assumption)
 
-g = Game()
+g = game.Game()
 will('You are at start', g.start(), 'start output')
 will('You cannot act unexpectedly', g.do('act unexpectedly'), 'unexpected action handling')
 will('You go to end', g.do('go to end'), 'expected action')
